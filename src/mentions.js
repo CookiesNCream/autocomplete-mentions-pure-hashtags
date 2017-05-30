66ac45df0b25eed50cb1f48ea8cc85dd36c208ec
Linda Lim <CookiesNCream@users.noreply.github.com>
2017-05-30 23:03:46 +0800

    Add files via upload

---
+++ src/mentions.js
@@ -0,0 +1,16 @@
+import { fromJS } from 'immutable';
+
+const mentions = fromJS([
+  {
+    name: 'Matthew Russell',
+    link: 'https://twitter.com/mrussell247',
+    avatar: 'https://pbs.twimg.com/profile_images/517863945/mattsailing_400x400.jpg',
+  },
+  {
+    name: 'Max Stoiber',
+    link: 'https://twitter.com/mxstbr',
+    avatar: 'https://pbs.twimg.com/profile_images/763033229993574400/6frGyDyA_400x400.jpg',
+  },
+]);
+
+export default mentions;
